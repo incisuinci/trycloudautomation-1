cccf1263faf9c39ec915e86d1a6752b9c60c767d
Jeren <jerenshamuhamedova@gmail.com>
2021-01-30 13:08:05 -0600

    first commit to github

---
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -0,0 +1,25 @@
+package com.trycloud.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties configFile;
+    static {
+        try {
+            String path = "configuration.properties";
+            FileInputStream input = new FileInputStream(path);
+            configFile = new Properties();
+            configFile.load(input);
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static String getProperty(String keyName) {
+        return configFile.getProperty(keyName);
+    }
+}
