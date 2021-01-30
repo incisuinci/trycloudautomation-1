f6d8363daef0aaa30c480ce3c5ee89251eb31142
esma17 <milkysf@gmail.com>
2021-01-30 14:26:48 -0600

    commit

--- src/test/java/com/trycloud/tests/Test.java
+++ src/test/java/com/trycloud/tests/Test.java
@@ -3,6 +3,8 @@ package com.trycloud.tests;
 public class Test {
     public static void main(String[] args) {
         System.out.println("B21");
+// everyone needs to creat a new branch INSIDE the Realese branch
 
+        // Jeren new change
     }
 }
