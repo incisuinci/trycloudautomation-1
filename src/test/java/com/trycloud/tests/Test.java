08f8986d31245fef854589ea4e5d96f5ae8e2d94
Jair Novello <jaja505@hotmail.com>
2021-02-02 18:00:08 -0500

    Add US002_TC001_Jair and fix the TestBase

--- src/test/java/com/trycloud/tests/Test.java
+++
@@ -1,9 +0,0 @@
-package com.trycloud.tests;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("B21");
-
-// Jair resolve the conflict 
-    }
-}
--- src/test/java/com/trycloud/tests/US002_TC001_Jair.java
+++ src/test/java/com/trycloud/tests/US002_TC001_Jair.java
@@ -1,4 +1,29 @@
 package com.trycloud.tests;
 
-public class US002_TC001_Jair {
+import com.trycloud.Pages.MainPagePOM;
+import com.trycloud.tests.base.TestBaseWithLogin;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class US002_TC001_Jair extends TestBaseWithLogin {
+
+    @Test
+    public void Test1 (){
+
+        MainPagePOM mainPagePOM = new MainPagePOM();
+        PageFactory.initElements(Driver.getDriver(),mainPagePOM);
+        mainPagePOM.ButtonActivityClick();
+
+
+
+
+
+
+
+
+    }
+
+
+
 }
