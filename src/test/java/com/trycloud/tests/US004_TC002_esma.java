858581a38504440812323cbfe5c06b014d1b8814
esma17 <milkysf@gmail.com>
2021-02-02 23:53:13 -0600

    modified TalkPagePOM.java by adding elements and creating a sending message method. in the test class I made the assertion

--- src/test/java/com/trycloud/tests/US004_TC002_esma.java
+++ src/test/java/com/trycloud/tests/US004_TC002_esma.java
@@ -1,31 +1,20 @@
 package com.trycloud.tests;
 
-import com.trycloud.Pages.MainPagePOM;
 import com.trycloud.Pages.TalkPagePOM;
 import com.trycloud.tests.base.TestBaseWithLogin;
-import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
-import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 public class US004_TC002_esma extends TestBaseWithLogin {
 
 
-    @Test
+    @Test(description = "Validation of sending message by searching user")
     public void t1() {
-        MainPagePOM mainPagePOM = new MainPagePOM();
         TalkPagePOM talkPagePOM = new TalkPagePOM();
-        PageFactory.initElements(Driver.getDriver(), mainPagePOM);
-        mainPagePOM.buttonTalk.click();
-        BrowserUtils.sleep(3);
-        PageFactory.initElements(Driver.getDriver(), talkPagePOM);
-
-        talkPagePOM.NexConversationText.click();
-        talkPagePOM.InputSearchBoxInTalk.sendKeys("User30");
-
-        BrowserUtils.sleep(3);
-
+        talkPagePOM.sendingMessageThroughTalk("User50");
+        Assert.assertEquals(talkPagePOM.messages.get(talkPagePOM.messages.size() - 2).getText(), talkPagePOM.message, "The message is not sent and displayed!!");
 
     }
 
