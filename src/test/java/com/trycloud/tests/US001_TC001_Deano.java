47fc7db4b87d7075569e0be85dcccd2d79583085
Dean Wynn <wynnkhalifa117@gmail.com>
2021-02-02 22:39:45 -0600

    In my US001_TC001_Deano class, I added the first steps in my test case fo test login info

--- src/test/java/com/trycloud/tests/US001_TC001_Deano.java
+++ src/test/java/com/trycloud/tests/US001_TC001_Deano.java
@@ -1,4 +1,34 @@
 package com.trycloud.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class US001_TC001_Deano {
+
+
+    @Test
+    public void testCase1U1(){
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://trycloud.net/");
+
+        driver.findElement(By.id("menu-item-47")).click();
+
+
+
+
+
+
+    }
+
+
+
 }
